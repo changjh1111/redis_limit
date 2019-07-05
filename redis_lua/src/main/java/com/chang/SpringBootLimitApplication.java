4b2f89667d061e0c80882d5b39909c13644e4e68
chang <changjh1111@163.com>
2019-07-05 14:43:19 +0800

    首次 分布式限流

---
+++ redis_lua/src/main/java/com/chang/SpringBootLimitApplication.java
@@ -0,0 +1,17 @@
+package com.chang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author chang
+ * @Date 2019/7/4
+ * @Description
+ */
+@SpringBootApplication
+public class SpringBootLimitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLimitApplication.class, args);
+    }
+}
